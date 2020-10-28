e17993f03d725ec2d4267794bf0d3a11dce66504
derekbrownh <59584928+derekbrownh@users.noreply.github.com>
2020-10-28 10:45:24 -0600

    Added Venmo Stuff

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { App} from "./App";
 import {SignIn, SignUp} from "./Authentication"
 import {Edit} from './EditScreen'
 import { BrowserRouter, Route} from "react-router-dom"
+import {Venmo} from './Venmo'
 
 ReactDOM.render(
     <BrowserRouter>
@@ -14,6 +15,7 @@ ReactDOM.render(
             <Route path = "/signup" component = {SignUp}/>
             <Route path = "/app" component = {App}/>
             <Route path = '/edit' component = {Edit} />
+            <Route path = '/venmo' component = {Venmo} />
         </div>
     </BrowserRouter>, 
     document.getElementById("root"));
