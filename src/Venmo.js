9e48e7d907e2d7ead94bb98f2fd53adcd73f5a2d
derekbrownh <59584928+derekbrownh@users.noreply.github.com>
2020-10-28 16:11:52 -0600

    Revert "removed venmo"
    This reverts commit 0ae74cee2e18405bf16aef7a89db9fcc48516a2e.

---
+++ src/Venmo.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+// import CardMedia from '@material-ui/core/CardMedia';
+import CardMedia from '@material-ui/core/CardMedia';
+
+export function Venmo() {  return (
+//         <CardMedia
+//     image="https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png"
+//     title="Paella dish"
+// />
+    // <div>
+    //     <img src="https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png" />
+    // </div>
+
+    // <div>
+    //     <img href="./derekVenmo.jpeg" />
+    // </div>
+    
+
+  );
+}
